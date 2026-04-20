179b52d237fd615022f2f3fb0479cddf7fe723e3
Jin Yang <ginyang6@gmail.com>
2026-04-19 22:59:06 -0400

    shell.c updated for backingStore

--- shell.c
+++ shell.c
@@ -1,92 +1,101 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h> 
+#include <string.h>
 #include <stdbool.h>
 #include <time.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 #include "interpreter.h"
 #include "shellmemory.h"
 #include "kernel.h"
 
+extern int frameSize;
+extern int varMemSize;
 
 int MAX_USER_INPUT = 1000;
 int parseInput(char ui[]);
 
 int main(int argc, char *argv[]) {
+    printf("%s\n", "Shell version 1.1 Created January 2022");
+    help();
 
-	printf("%s\n", "Shell version 1.1 Created January 2022");
-	help();
+    char prompt = '$';
+    char userInput[MAX_USER_INPUT];
+    int errorCode = 0;
 
-	char prompt = '$';  				// Shell prompt
-	char userInput[MAX_USER_INPUT];		// user's input stored here
-	int errorCode = 0;					// zero means no error, default
+    printf("Frame Store Size = %d; Variable Store Size = %d\n", frameSize, varMemSize);
 
-	//init user input
-	for (int i=0; i<MAX_USER_INPUT; i++)
-		userInput[i] = '\0';
-	
-	//init shell memory
-	mem_init();
+    errno = 0;
+    int ret = mkdir("BackingStore", S_IRWXU);
+    if (ret == -1 && errno == EEXIST) {
+        system("rm -rf BackingStore");
+        mkdir("BackingStore", S_IRWXU);
+    }
 
-	ready_queue_initialize();
+    for (int i = 0; i < MAX_USER_INPUT; i++) {
+        userInput[i] = '\0';
+    }
 
-	//initialize random number generator, for fileID and pid generation
-	srand(time(NULL));
-	
-	while(1) {						
-		printf("%c ",prompt);
-		fgets(userInput, MAX_USER_INPUT-1, stdin);
+    mem_init();
+    ready_queue_initialize();
+    srand(time(NULL));
 
-		if (feof(stdin)){
-			freopen("/dev/tty", "r", stdin);
-		}	
-		
-		errorCode = parseInput(userInput);
-		if (errorCode == -1) exit(99);	// ignore all other errors
-		memset(userInput, 0, sizeof(userInput));
-	}
+    while (1) {
+        printf("%c ", prompt);
+        fgets(userInput, MAX_USER_INPUT - 1, stdin);
 
-	return 0;
+        if (feof(stdin)) {
+            freopen("/dev/tty", "r", stdin);
+        }
 
+        errorCode = parseInput(userInput);
+        if (errorCode == -1) exit(99);
+        memset(userInput, 0, sizeof(userInput));
+    }
+
+    return 0;
 }
 
 int parseInput(char ui[]) {
-	char tmp[200];
-	char *words[100];							
-	int a = 0;
-	int b;							
-	int w=0; // wordID	
-	int errorCode;
-	for(a=0; ui[a]==' ' && a<1000; a++);		// skip white spaces
+    char tmp[200];
+    char *words[100];
+    int a = 0;
+    int b;
+    int w = 0;
+    int errorCode;
 
-	while(ui[a] != '\n' && ui[a] != '\0' && a<1000) {
-		for(b=0; ui[a]!=';' && ui[a]!='\0' && ui[a]!='\n' && ui[a]!=' ' && a<1000; a++, b++)
-			tmp[b] = ui[a];						// extract a word
-		tmp[b] = '\0';
+    for (a = 0; ui[a] == ' ' && a < 1000; a++);
 
-		words[w] = strdup(tmp);
+    while (ui[a] != '\n' && ui[a] != '\0' && a < 1000) {
+        for (b = 0; ui[a] != ';' && ui[a] != '\0' && ui[a] != '\n' && ui[a] != ' ' && a < 1000; a++, b++) {
+            tmp[b] = ui[a];
+        }
+        tmp[b] = '\0';
 
-		if(ui[a]==';'){
-			w++;
+        words[w] = strdup(tmp);
 
-			errorCode = interpreter(words, w);
-			if(errorCode == -1){
-				return errorCode;
-			}
+        if (ui[a] == ';') {
+            w++;
+            errorCode = interpreter(words, w);
+            if (errorCode == -1) {
+                return errorCode;
+            }
 
-			a++;
-			w = 0;
-			for(; ui[a]==' ' && a<1000; a++);		// skip white spaces
-			continue;
-		}
-		
-		w++;
-        if(ui[a] == '\0'){
+            a++;
+            w = 0;
+            for (; ui[a] == ' ' && a < 1000; a++);
+            continue;
+        }
+
+        w++;
+        if (ui[a] == '\0') {
             break;
         }
-        a++; 
-	}
-	errorCode = interpreter(words, w);
+        a++;
+    }
 
-	return errorCode;
+    errorCode = interpreter(words, w);
+    return errorCode;
 }
--- shellmemory.h
+++ shellmemory.h
@@ -1,9 +1,16 @@
-#include<stdlib.h>
-#include<stdio.h>
+#ifndef SHELLMEMORY_H
+#define SHELLMEMORY_H
+
+#include <stdio.h>
 
 void mem_init();
 char *mem_get_value(char *var);
-char* mem_get_value_by_line(int line);
 void mem_set_value(char *var, char *value);
-void clean_mem(int start, int end);
-int add_file_to_mem(FILE* fp, int* pStart, int* pEnd, char* fileID);
+
+void clean_mem(int frameNum);
+int load_page_to_mem(FILE* fp, int pageNum, int frameNum, char* fileID);
+int findFrame();
+int checkFrame(int frameNum, int offset);
+char* getFrameLine(int frameNum, int offset);
+
+#endif
