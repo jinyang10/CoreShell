f615a0653db1980cf3aafd2f39a6ee20400e470f
Jin Yang <ginyang6@gmail.com>
2026-04-19 22:49:40 -0400

    defined FS and VS

--- shellmemory.c
+++ shellmemory.c
@@ -1,138 +1,133 @@
-#include<stdlib.h>
-#include<string.h>
-#include<stdio.h>
-#include<stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdbool.h>
 
 #define SHELL_MEM_LENGTH 1000
+#define FS 500
+#define VS 500
 
-struct memory_struct{
-	char *var;
-	char *value;
+int frameSize = FS;
+int varMemSize = VS;
+
+struct memory_struct {
+    char *var;
+    char *value;
 };
 
-struct memory_struct shellmemory[SHELL_MEM_LENGTH];
+struct frame_struct {
+    struct memory_struct frame[3];
+};
 
-// Shell memory functions
+struct frame_struct frameStore[500];
+struct memory_struct varStore[500];
 
-void mem_init(){
+size_t sizeVarStore = 500;
+size_t sizeFrameStore = 500;
 
-	int i;
-	for (i=0; i<SHELL_MEM_LENGTH; i++){		
-		shellmemory[i].var = "none";
-		shellmemory[i].value = "none";
-	}
+// check frame slot at frameNum, offset; 1 if occupied, 0 if not
+int checkFrame(int frameNum, int offset) {
+    if (strcmp(frameStore[frameNum].frame[offset].var, "none") == 0) {
+        return 0;
+    }
+    return 1;
 }
 
-// Set key value pair
+char* getFrameLine(int frameNum, int offset) {
+    return strdup(frameStore[frameNum].frame[offset].value);
+}
+
+void resetmem() {
+    for (int i = 0; i < sizeVarStore; i++) {
+        varStore[i].var = "none";
+        varStore[i].value = "none";
+    }
+}
+
+void mem_init() {
+    for (int i = 0; i < sizeFrameStore; i++) {
+        for (int j = 0; j < 3; j++) {
+            frameStore[i].frame[j].var = "none";
+            frameStore[i].frame[j].value = "none";
+        }
+    }
+    resetmem();
+}
+
 void mem_set_value(char *var_in, char *value_in) {
+    for (int i = 0; i < sizeVarStore; i++) {
+        if (strcmp(varStore[i].var, var_in) == 0) {
+            varStore[i].value = strdup(value_in);
+            return;
+        }
+    }
 
-	int i;
-
-	for (i=0; i<SHELL_MEM_LENGTH; i++){
-		if (strcmp(shellmemory[i].var, var_in) == 0){
-			shellmemory[i].value = strdup(value_in);
-			return;
-		} 
-	}
-
-	//Value does not exist, need to find a free spot.
-	for (i=0; i<SHELL_MEM_LENGTH; i++){
-		if (strcmp(shellmemory[i].var, "none") == 0){
-			shellmemory[i].var = strdup(var_in);
-			shellmemory[i].value = strdup(value_in);
-			return;
-		} 
-	}
-
-	return;
-
+    for (int i = 0; i < sizeVarStore; i++) {
+        if (strcmp(varStore[i].var, "none") == 0) {
+            varStore[i].var = strdup(var_in);
+            varStore[i].value = strdup(value_in);
+            return;
+        }
+    }
 }
 
-//get value based on input key
 char *mem_get_value(char *var_in) {
-	int i;
-
-	for (i=0; i<SHELL_MEM_LENGTH; i++){
-		if (strcmp(shellmemory[i].var, var_in) == 0){
-			return strdup(shellmemory[i].value);
-		} 
-	}
-	return "Variable does not exist";
-
-}
-
-char* mem_get_value_by_line(int line){
-	return shellmemory[line].value;
-}
-
-
-void clean_mem(int start, int end){
-    for(int i = start; i <= end; i ++){
-        shellmemory[i].var = "none";
-		shellmemory[i].value = "none";
-    }
-}
-
-/*
- * Function:  addFileToMem 
- * 	Added in A2
- * --------------------
- * Load the source code of the file fp into the shell memory:
- * 		Loading format - var stores fileID, value stores a line
- *		Note that the first 100 lines are for set command, the rests are for run and exec command
- *
- *  pStart: This function will store the first line of the loaded file 
- * 			in shell memory in here
- *	pEnd: This function will store the last line of the loaded file 
- 			in shell memory in here
- *  fileID: Input that need to provide when calling the function, 
- 			stores the ID of the file
- * 
- * returns: error code, 21: no space left
- */
-int add_file_to_mem(FILE* fp, int* pStart, int* pEnd, char* fileID)
-{
-    char line[SHELL_MEM_LENGTH];
-    size_t i;
-    int error_code = 0;
-	bool hasSpaceLeft = false;
-
-    for (i = 100; i < SHELL_MEM_LENGTH; i++){
-        if(strcmp(shellmemory[i].var,"none") == 0){
-            *pStart = (int)i;
-			hasSpaceLeft = true;
-            break;
+    for (int i = 0; i < sizeVarStore; i++) {
+        if (strcmp(varStore[i].var, var_in) == 0) {
+            return strdup(varStore[i].value);
         }
     }
+    return "Variable does not exist";
+}
 
-	//shell memory is full
-	if(hasSpaceLeft == 0){
-		error_code = 21;
-		return error_code;
-	}
-    
-    for (size_t j = i; j < SHELL_MEM_LENGTH; j++){
-        if(feof(fp))
-        {
-            *pEnd = (int)j-1;
-            break;
-        }else{
-            fgets(line, 999, fp);
-			shellmemory[j].var = strdup(fileID);
-            shellmemory[j].value = strdup(line);
-        }
+void clean_mem(int frameNum) {
+    for (int i = 0; i < 3; i++) {
+        frameStore[frameNum].frame[i].var = "none";
+        frameStore[frameNum].frame[i].value = "none";
+    }
+}
+
+int load_page_to_mem(FILE* fp, int pageNum, int frameNum, char* fileID) {
+    rewind(fp);
+
+    char line[1000];
+    char buffer[1000];
+    int i = 0;
+
+    // skip previous pages
+    while (i < pageNum * 3 && fgets(buffer, sizeof(buffer), fp) != NULL) {
+        i++;
     }
 
-	//no space left to load the entire file into shell memory
-	if(!feof(fp)){
-		error_code = 21;
-		//clean up the file in memory
-		for(int j = 1; i <= SHELL_MEM_LENGTH; i ++){
-			shellmemory[j].var = "none";
-			shellmemory[j].value = "none";
-    	}
-		return error_code;
-	}
+    // load up to 3 lines into the SAME frame
+    i = 0;
+    while (i < 3 && fgets(line, sizeof(line), fp) != NULL) {
+        frameStore[frameNum].frame[i].value = strdup(line);
+        frameStore[frameNum].frame[i].var = strdup(fileID);
+        i++;
+    }
 
-    return error_code;
+    while (i < 3) {
+        frameStore[frameNum].frame[i].value = "none";
+        frameStore[frameNum].frame[i].var = "none";
+        i++;
+    }
+
+    return 0;
+}
+
+int findFrame() {
+    for (int i = 0; i < sizeFrameStore; i++) {
+        int freeFrame = 1;
+        for (int j = 0; j < 3; j++) {
+            if (strcmp(frameStore[i].frame[j].var, "none") != 0) {
+                freeFrame = 0;
+                break;
+            }
+        }
+        if (freeFrame) {
+            return i;
+        }
+    }
+    return -1;
 }
